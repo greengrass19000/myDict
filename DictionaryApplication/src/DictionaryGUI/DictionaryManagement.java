6ca744b7bb4d8574136ac29d808d5367e39f4f59
ngot23 <levuquang2k1@gmail.com>
2020-10-06 15:09:17 +0700

    Init

---
+++ DictionaryApplication/src/DictionaryGUI/DictionaryManagement.java
@@ -0,0 +1,53 @@
+package DictionaryGUI;
+import java.util.*;
+import java.io.*;
+
+public class DictionaryManagement 
+{
+    private Dictionary dictionary;
+
+    // constructors
+    public DictionaryManagement() 
+    {
+        dictionary = new Dictionary();
+    }
+
+    public DictionaryManagement(Dictionary d) 
+    {
+        dictionary = d;
+    }
+
+    /**
+     *
+     * @throws IOException
+     */
+    public void insertFromFile() throws IOException 
+    {
+        File fileDir = new File("dictionaries.txt");
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fileDir), "UTF8"));
+        while (br.ready()) {
+            String lineWord = br.readLine();
+            String[] parts = lineWord.split("\\t");
+            if (parts.length == 2) {
+                dictionary.insertWord(parts[0], parts[1]);
+            }
+        }
+        br.close();    
+    }
+    public void dictionaryExportToFile() throws IOException
+    {
+        File fileDir = new File("DictionariesUpdated.txt");
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileDir), "UTF8"));
+        ArrayList<Word> dict = dictionary.getDict();
+        for(Word w : dict) {
+            String en = w.getWordTarget();
+            String vi = w.getWordExplain();
+            String result = en + "\t" + "<html>" + vi + "</html>";
+            result = FormatOutput.formatLine(result);
+            writer.write(result);
+            writer.newLine();
+        }
+        writer.close();
+    }
+
+}
